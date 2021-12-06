95d7002bc6a2419f228de005f07ce9b1e9bb2b30
Vaschukalex <alex2003coop@gmail.com>
2021-12-06 01:49:45 +0100

    Dev tool

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -3,8 +3,7 @@ import reducers from "./reducers/index";
 
 const store = createStore(reducers,
     (localStorage['favorite-characters']) ? JSON.parse(localStorage['favorite-characters']) :
-        {},
-    window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+        {}
 );
 store.subscribe(() => {
     localStorage['favorite-characters'] = JSON.stringify(store.getState());
